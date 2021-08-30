5944dce15d58f2000a4384fa2052017de61df4b0
neviton viana <nevitonviana1@hotmail.com>
2021-08-30 19:01:00 -0300

    criação do app Game Play

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'GamePlay',
+    theme: ThemeData(
+
+      primarySwatch: Colors.blue,
+    ),
+    home: ,
+  ),);
+}
+
