73cf7d8c540c60846b0ae631d28937556d3c9f9c
neviton viana <nevitonviana1@hotmail.com>
2021-08-31 19:37:22 -0300

    construido o button de login

---
+++ lib/components/widget/button/buttonCustom.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '/components/themas/themas.dart';
+
+class ButtonCustom extends StatelessWidget {
+  const ButtonCustom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: ThemasColors.redButton,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      width: 275,
+      height: 56,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Images.discord,
+          ),
+          Container(
+            height: 56,
+            width: 1,
+            color: Colors.black38,
+          ),
+          Expanded(
+            flex: 4,
+            child: Text(
+              "Entrar com Discord",
+              style: TextStyles.fontsInter500,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
